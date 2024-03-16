a29b40e4f5fd9208579decd9f82f510e3d848ee1
Luis César Lizama Quiñónez <ElLuisLizama25@gmail.com>
2024-03-16 17:34:02 -0600

    feat: Implement 'errors' report

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def save_file():
 def execute_code():
     input_code = code_editor.get("1.0", tk.END)  # Se obtiene el código del editor de texto.
 
-    environment = Environment(None, "Global")  # Se crea un entorno global.
+    environment = Environment(None, "global")  # Se crea un entorno global.
     interpreter = Interpreter()
     syntax_tree = SyntaxTree()
 
@@ -51,9 +51,69 @@ def execute_code():
     console_tab.insert("1.0", str(syntax_tree.get_console()))  # Se inserta el contenido de la consola.
     console_tab.config(state="disabled")  # Se deshabilita la pestaña de consola.
 
+    errors_tab.config(state="normal")  # Se habilita la pestaña de errores.
+    errors_tab.delete("1.0", tk.END)  # Se borra el contenido de la pestaña de errores.
+    errors_tab.insert("1.0", format_errors(syntax_tree.get_errors()))
+    errors_tab.config(state="disabled")  # Se deshabilita la pestaña de errores.
+
 
 def show_reports():
     messagebox.showinfo("Reportes", "Funcionalidad de informes no implementada.")
+
+
+def format_errors(errors):
+    """Se guardan los campos de los errores en listas separadas, incluyendo el encabezado para cada campo."""
+
+    descriptions = [error.description for error in errors] + ["Descripción"]
+    scopes = [error.scope for error in errors] + ["Entorno"]
+    lines = [str(error.line) for error in errors] + ["Línea"]
+    columns = [str(error.column) for error in errors] + ["Columna"]
+    kinds = [error.kind for error in errors] + ["Tipo de Error"]
+
+    """Se calcula el ancho máximo para cada columna."""
+
+    max_lens = {
+        "description": max(map(len, descriptions)),
+        "scope": max(map(len, scopes)),
+        "line": max(map(len, lines)),
+        "column": max(map(len, columns)),
+        "kind": max(map(len, kinds)),
+    }
+
+    """Se le da formato a los encabezados de las columnas."""
+
+    header_format = "{:{description}} | {:{scope}} | {:{line}} | {:{column}} | {:{kind}}"
+
+    header = header_format.format(
+        "Descripción".center(max_lens["description"]),
+        "Entorno".center(max_lens["scope"]),
+        "Línea".center(max_lens["line"]),
+        "Columna".center(max_lens["column"]),
+        "Tipo de Error".center(max_lens["kind"]),
+        **max_lens
+    )
+
+    separator = '-' * len(header)  # Se crea un separador para los encabezados basado en el ancho máximo.
+
+    formatted_errors = [header, separator]  # Se inicializa la lista de errores con los encabezados y el separador.
+
+    """Se le da formato a cada error."""
+
+    error_format = "{:{description}} | {:{scope}} | {:{line}} | {:{column}} | {:{kind}}"
+
+    for error in errors:
+        formatted_error = error_format.format(
+            error.description.center(max_lens["description"]),
+            error.scope.center(max_lens["scope"]),
+            str(error.line).center(max_lens["line"]),
+            str(error.column).center(max_lens["column"]),
+            error.kind.center(max_lens["kind"]),
+            **max_lens
+        )
+
+        formatted_errors.append(formatted_error)
+
+    return "\n".join(formatted_errors)
 
 
 root = tk.Tk()  # Se crea la ventana principal.
@@ -90,11 +150,11 @@ report_button.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
 tab_control = ttk.Notebook(root)  # Se crea un control de pestañas.
 
 console_tab = scrolledtext.ScrolledText(tab_control, wrap=tk.WORD)  # Se crea una pestaña para la consola.
-console_tab.config(state="disabled")
+console_tab.config(state="disabled", wrap=tk.NONE, font=("Courier", 10))
 symbol_tab = tk.Text(tab_control)  # Se crea una pestaña para la tabla de símbolos.
-symbol_tab.config(state="disabled")
+symbol_tab.config(state="disabled", wrap=tk.NONE, font=("Courier", 7))
 errors_tab = tk.Text(tab_control)  # Se crea una pestaña para los errores.
-errors_tab.config(state="disabled")
+errors_tab.config(state="disabled", wrap=tk.NONE, font=("Courier", 7))
 
 tab_control.add(console_tab, text="Consola")
 tab_control.add(symbol_tab, text="Tabla de Símbolos")
